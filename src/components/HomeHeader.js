50fc9a60430fcd2de27d01a2263cd9bbbb8b4ff7
unknown <dayvynomern2021@gmail.com>
2023-08-20 11:52:45 +0100

    Added sliding animation to the modal

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -6,10 +6,32 @@ import { useNavigate } from 'react-router-dom';
 
 const HomeHeader = () => {
   
+  //Handle the opening and clsoing of the modal overlay
   const [openModal, setOpenModal] = React.useState(false);
+
+  //Handles the sliding in of the modal 
+  const [animateModal, setAnimateModal] = React.useState(null);
   
-  const handleModal = () => {
-    setOpenModal(prev => !prev);
+/**
+ * @returns void: The function activates the modal overlay via sliding in through the top
+ */
+  const handleModalOpen = () => {
+    setOpenModal(true);
+
+    setTimeout(() => {
+      setAnimateModal(true);
+    }, 1)
+  }
+
+  /**
+   * @returns void: The function deactivates the modal via sliding out through the top
+   */
+  const handleModalClose = () => {
+    setAnimateModal(false);
+    
+    setTimeout(() => {
+      setOpenModal(false);
+    }, 20)
   }
   
   const navigate = useNavigate();
@@ -27,15 +49,15 @@ const HomeHeader = () => {
         <button onClick={navigateHome}>
           <img src={'images/logo.png'} alt="" />
         </button>
-        <button onClick={handleModal} className={openModal? "btn--open" : "btn--close"} >&nbsp;</button>
+        <button onClick={handleModalOpen} onAnimationEnd={()=>setAnimateModal(null)} className={openModal? "btn--open" : "btn--close"} >&nbsp;</button>
       </div>
       {
         openModal && (
         <Modal>
-          <div className="modal">
-            <div className="mobile__header">
+          <div className="modal" >
+            <div className={animateModal==true? "mobile__header" : "initial__header"}>
               <Header routes={routes} />
-              {openModal && <button onClick={handleModal} className="close__mobile"> <img src="images/close.svg" alt="" /> </button> }
+              {openModal && <button onAnimationEnd={()=>setAnimateModal(null)} onClick={handleModalClose} className="close__mobile"> <img src="images/close.svg" alt="" /> </button> }
             </div>
           </div>
         </Modal>
